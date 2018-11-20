c8001baa9cd452f8ca5b69fb7395eabf6951c7ca
unknown <mabbasidesign@yahoo.com>
2018-11-19 21:06:12 -0700

    create home page animation

--- man-city/src/routes.js
+++ man-city/src/routes.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Layout from './Hoc/Layout';
+import { Switch, Route } from 'react-router-dom';
 
-const Routes = (props) =>{
+import Home from './Components/Home'
+
+const Routes = () =>{
   return (
     <div>
       <Layout>
-        second line
+        <Switch>
+            <Route exact component={Home} path="/" />
+        </Switch>
       </Layout>
     </div>
   )
