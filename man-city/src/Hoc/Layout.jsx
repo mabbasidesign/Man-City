f375dbdc8b34c7f7aab85b732b12ab74d065c1c3
unknown <mabbasidesign@yahoo.com>
2018-11-19 17:02:49 -0700

    create footer

--- man-city/src/Hoc/Layout.jsx
+++ man-city/src/Hoc/Layout.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Header from '../Components/Header-Footer/Header';
+import Footer from '../Components/Header-Footer/Footer';
 
 const Layout = (props) => {
     return ( 
         <div>
             <Header />
             { props.children }
+            <Footer />
         </div>
      );
 }
