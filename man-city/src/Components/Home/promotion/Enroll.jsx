15139f4718e5d5fb490675e06a9be21d07964beb
unknown <mabbasidesign@yahoo.com>
2018-11-26 22:51:21 -0700

    starting form enroll

--- man-city/src/Components/Home/promotion/Enroll.jsx
+++ man-city/src/Components/Home/promotion/Enroll.jsx
@@ -1,11 +1,55 @@
 import React, { Component } from 'react';
+import Fade from 'react-reveal';
+import FormField from '../../ui/formFields';
 
-const Enrol = () => {
-    return ( 
-        <div>
-            Enrollment
-        </div>
-     );
+class Enroll extends Component {
+    state = { 
+        formErrror: false,
+        formSuccess: '',
+        formdata: {
+            email: {
+                element: 'input',
+                value: '',
+                config:{
+                    name: 'email-input',
+                    type: 'email',
+                    placeholder: 'Enter Your email'
+                },
+                validation:{
+                    required: true,
+                    email: true
+                },
+                valid: false,
+                validationMessage: ''
+            }
+        }
+    }
+
+
+    submitForm = () => {
+
+    }
+
+
+    render() { 
+        return ( 
+            <Fade>
+                <div className='enroll_wrapper'>
+                    <form onSubmit={ (event) => this.submitForm(event) }>
+                        <div className='enroll_title'>
+                            Enter your email
+                        </div>
+                        <div className='enroll_input'>
+                            <FormField
+                            id={'email'}
+                            formdata={this.state.formdata.email}
+                            />
+                        </div>
+                    </form>
+                </div>
+            </Fade>
+         );
+    }
 }
- 
-export default Enrol;
+
+export default Enroll;
